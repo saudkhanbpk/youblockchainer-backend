b0f884476e10c252a07b9849eef554b675919c1d
Vidhan Shah <shahvidhan123@gmail.com>
2023-04-18 17:16:23 +0530

    api: auth and multer middlewares

---
+++ middleware/auth.js
@@ -0,0 +1,95 @@
+const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+
+const isAdmin = () => async (req, res, next) => {
+  if(req.user && req.user.isAdmin) {
+    next();
+  } else {
+    return res.status(401).json({
+      msg: 'Not an admin',
+    });
+  }
+}
+
+const cookieAuthRequired = () => async (req, res, next) => {
+  let token = req.cookies.access_token;
+  if(req.header && req.header('Authorization')) {
+    token = req.header('Authorization').replace('Bearer ', '');
+  }
+  console.log(token);
+  if (!token) {
+    return res.status(401).json({
+      msg: 'Unauthorized',
+    });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+   
+    if (!decoded) {
+      return res.status(401).json({
+        msg: 'Invalid token',
+      });
+    }
+   
+    let user = await User.findOne({ _id: decoded.id });
+    
+    if (!user) {
+      return res.status(404).json(`User not found!`);
+    }
+
+    req.token = token;
+    req.user = user;
+    res.locals.user = user;
+    next();
+  } catch {
+    return res.status(401).json({
+      msg: 'Invalid token',
+    });
+  }
+}
+
+/**
+ * Requires a token in request headers.
+ * Header format is
+ * Authorization: Bearer token
+ */
+const authRequired = () => async (req, res, next) => {
+  const header = req.header('Authorization');
+   
+  if (!header) {
+    return res.status(401).json({
+      msg: 'Please Provide JWT',
+    });
+  }
+  
+  const token = header.replace('Bearer', '').trim();
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+   
+    if (!decoded) {
+      return res.status(401).json({
+        msg: 'Invalid token',
+      });
+    }
+   
+    let user = await User.findOne({ _id: decoded.id }).select({
+      password: 0,
+    })
+    
+    if (!user) {
+      return res.status(404).json(`User not found!`);
+    }
+
+    req.token = token;
+    req.user = user;
+    res.locals.user = user;
+    next();
+  
+  } catch (e) {
+    return res.status(401).json({
+      msg: 'Invalid token',
+    });
+  }
+};
+
+module.exports = { authRequired, cookieAuthRequired, isAdmin };
---
+++ middleware/multer.js
@@ -0,0 +1,30 @@
+const multer = require("multer");
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 20 * 1024 * 1024, // 20MB
+  },
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+});
+
+function checkFileType(file, cb) {
+  // // Allowed ext
+  // const filetypes = /jpeg|jpg|png|gif|mp4|mov|wmv|flv|avi|webm|mkv|pdf/;
+  // // Check ext
+  // const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+
+  // // Check mime
+  // const mimetype = filetypes.test(file.mimetype);
+
+  // if (mimetype && extname) {
+  //   return cb(null, true);
+  // } else {
+  //   cb("Error: Only jpeg, jpg, png, gif, mp4, mov, wmv, flv, avi, webm, mkv, pdf files.");
+  // }
+  return cb(null, true);
+}
+
+module.exports = { upload };
