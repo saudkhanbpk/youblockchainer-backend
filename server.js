a121382675cc20a3827f5f6dbd6e07ee4301dbfb
Vidhan Shah <shahvidhan123@gmail.com>
2023-05-23 12:52:50 +0530

    api: chat creation logic change

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ const userRouter = require('./routes/userRouter');
 const brandRouter = require('./routes/brandRouter');
 const gptRouter = require('./routes/gptRouter');
 const ipfsRouter = require('./routes/ipfsRouter');
-const chatRouter = require("./routes/chatRouter");
+const chatRouter = require('./routes/chatRouter');
 
 // Dev middleware Morgan
 if (process.env.NODE_ENV === 'development') {
@@ -62,7 +62,7 @@ app.use('/api/v1/user', userRouter);
 app.use('/api/v1/brand', brandRouter);
 app.use('/api/v1/gpt', gptRouter);
 app.use('/api/v1/ipfs', ipfsRouter);
-app.use("/api/v1/chat", chatRouter);
+app.use('/api/v1/chat', chatRouter);
 
 // Handling other routes
 app.get('*', (req, res) => {
@@ -81,46 +81,41 @@ const server = app.listen(
 );
 
 // Web Sockets
-const io = require("socket.io")(server);
+const io = require('socket.io')(server);
 
-const Chat = require("./models/chatModel");
+const Chat = require('./models/chatModel');
 
-io.on("connection", (socket) => {
+io.on('connection', (socket) => {
   // Join Room
-  socket.on("Join Room", (roomId) => {
+  socket.on('Join Room', (roomId) => {
     socket.join(roomId);
     console.log(`User with ID: ${socket.id} joined room: ${roomId}`);
   });
 
-  socket.on("Input Chat Message", async (msg) => {
+  socket.on('Input Chat Message', async (msg) => {
     try {
       let chat;
 
       if (msg.roomId) {
-        chat = new Chat({
+        chat = await Chat.create({
           chatMessage: msg.chatMessage,
           sender: msg.sender,
           type: msg.type,
           roomId: msg.roomId,
         });
-      }
 
-      chat.save((err, doc) => {
-        if (err) console.log(err);
-        Chat.findById(doc._id)
-          .populate("sender")
-          .exec((err, doc) => {
-            return socket.to(msg.roomId).emit("Output Chat Message", doc);
-            // return io.emit("Output Chat Message", doc);
-          });
-      });
+        const doc = await Chat.findById(chat._id).populate('sender');
+
+        return socket.to(msg.roomId).emit('Output Chat Message', doc);
+        // return io.emit("Output Chat Message", doc);
+      }
     } catch (error) {
       console.log(error);
     }
   });
 
-  socket.on("disconnect", () => {
-    console.log("User Disconnected", socket.id);
+  socket.on('disconnect', () => {
+    console.log('User Disconnected', socket.id);
   });
 });
 
