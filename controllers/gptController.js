9ce561684c676db6ccb48c5431d5a08337616ffc
Vidhan Shah <shahvidhan123@gmail.com>
2023-09-15 19:42:55 +0530

    api: changes for buying scripts

--- controllers/gptController.js
+++ controllers/gptController.js
@@ -2,12 +2,12 @@ const { get_response } = require("../utils/gpt");
 const { getMethods } = require('../config/blockchain');
 
 exports.askChatGPT = async (req, res) => {
-  // const { mainC } = await getMethods();
-  // const num = await mainC.getPendingScripts(req.user.walletAddress);
+  const { mainC } = await getMethods();
+  const num = await mainC.getPendingScripts(req.user.walletAddress);
 
-  // if(num <= 0) {
-  //   return res.status(401).json('Script limit exceeded, buy more to continue');
-  // }
+  if(num <= 0) {
+    return res.status(401).json('Script limit exceeded, buy more to continue');
+  }
 
   const { prompt, isLast } = req.body;
   if (!prompt) {
@@ -16,9 +16,9 @@ exports.askChatGPT = async (req, res) => {
   const resp = await get_response(prompt);
   res.status(200).json(resp);
 
-  // if(isLast) {
-  //   await (await mainC.deductPendingScripts(req.user.walletAddress, { value: 0 })).wait();
-  // }
+  if(isLast) {
+    await (await mainC.deductPendingScripts(req.user.walletAddress, { value: 0 })).wait();
+  }
 };
 
 exports.finishGeneration = async (req, res) => {
