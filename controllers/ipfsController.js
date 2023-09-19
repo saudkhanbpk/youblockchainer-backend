318093c023e9746a1f58e84bdbad74455086060d
Vidhan Shah <shahvidhan123@gmail.com>
2023-09-19 14:52:52 +0530

    api: restructure ipfs components

--- controllers/ipfsController.js
+++ controllers/ipfsController.js
@@ -1,51 +1,63 @@
 const { getMethods } = require('../config/blockchain');
 
 exports.uploadImg = async (req, res) => {
-  try {
-    if (!req.files) {
-      res.statusCode = 400;
-      res.setHeader('Content-Type', 'application/json');
-      res.json({
-        success: false,
-        status: 'Not a single file found in request',
-      });
-    } else {
-      const { ipfs } = await getMethods();
-      let urls = [];
-      await Promise.all(
-        req.files.map(async (fil) => {
-          let CID = await ipfs.add(fil.buffer);
-          urls.push(`https://youblockchainer.infura-ipfs.io/ipfs/${CID.path}`);
-        })
-      );
-
-      res.json({
-        success: true,
-        urls,
-      });
-    }
-  } catch (err) {
+  if (!req.files) {
+    res.statusCode = 400;
+    res.setHeader('Content-Type', 'application/json');
     res.json({
       success: false,
-      msg: err.message,
+      status: 'Not a single file found in request',
+    });
+  } else {
+    const auth =
+      `Basic ` +
+      Buffer.from(
+        process.env.PROJECT_ID + `:` + process.env.PROJECT_SECRET
+      ).toString(`base64`);
+    const ipfs = ipfsClient({
+      host: 'ipfs.infura.io',
+      port: 5001,
+      protocol: 'https',
+      headers: {
+        authorization: auth,
+      },
+    });
+    // const { ipfs } = await getMethods();
+    let urls = [];
+    await Promise.all(
+      req.files.map(async (fil) => {
+        let CID = await ipfs.add(fil.buffer);
+        urls.push(`https://youblockchainer.infura-ipfs.io/ipfs/${CID.path}`);
+      })
+    );
+
+    res.json({
+      success: true,
+      urls,
     });
   }
 };
 
 exports.uploadJson = async (req, res) => {
-  try {
-    const { ipfs } = await getMethods();
-    let CID = await ipfs.add(JSON.stringify(req.body));
-    let url = `https://youblockchainer.infura-ipfs.io/ipfs/${CID.path}`;
+  const auth =
+      `Basic ` +
+      Buffer.from(
+        process.env.PROJECT_ID + `:` + process.env.PROJECT_SECRET
+      ).toString(`base64`);
+    const ipfs = ipfsClient({
+      host: 'ipfs.infura.io',
+      port: 5001,
+      protocol: 'https',
+      headers: {
+        authorization: auth,
+      },
+    });
+  // const { ipfs } = await getMethods();
+  let CID = await ipfs.add(JSON.stringify(req.body));
+  let url = `https://youblockchainer.infura-ipfs.io/ipfs/${CID.path}`;
 
-    res.json({
-      success: true,
-      url,
-    });
-  } catch (err) {
-    res.json({
-      success: false,
-      msg: err.message,
-    });
-  }
+  res.json({
+    success: true,
+    url,
+  });
 };
