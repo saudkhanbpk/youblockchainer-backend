8736e7f4c7e1dba4a56a5cb5e55c5e5dde723a00
Vidhan Shah <shahvidhan123@gmail.com>
2023-09-19 14:57:38 +0530

    api: restructure ipfs components revert

--- controllers/ipfsController.js
+++ controllers/ipfsController.js
@@ -9,20 +9,7 @@ exports.uploadImg = async (req, res) => {
       status: 'Not a single file found in request',
     });
   } else {
-    const auth =
-      `Basic ` +
-      Buffer.from(
-        process.env.PROJECT_ID + `:` + process.env.PROJECT_SECRET
-      ).toString(`base64`);
-    const ipfs = ipfsClient({
-      host: 'ipfs.infura.io',
-      port: 5001,
-      protocol: 'https',
-      headers: {
-        authorization: auth,
-      },
-    });
-    // const { ipfs } = await getMethods();
+    const { ipfs } = await getMethods();
     let urls = [];
     await Promise.all(
       req.files.map(async (fil) => {
@@ -39,20 +26,7 @@ exports.uploadImg = async (req, res) => {
 };
 
 exports.uploadJson = async (req, res) => {
-  const auth =
-      `Basic ` +
-      Buffer.from(
-        process.env.PROJECT_ID + `:` + process.env.PROJECT_SECRET
-      ).toString(`base64`);
-    const ipfs = ipfsClient({
-      host: 'ipfs.infura.io',
-      port: 5001,
-      protocol: 'https',
-      headers: {
-        authorization: auth,
-      },
-    });
-  // const { ipfs } = await getMethods();
+  const { ipfs } = await getMethods();
   let CID = await ipfs.add(JSON.stringify(req.body));
   let url = `https://youblockchainer.infura-ipfs.io/ipfs/${CID.path}`;
 
