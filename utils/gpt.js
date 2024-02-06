de5d0f2333af1f3872d1a9d1bfdf6d5d34a9b094
Vidhan123 <shahvidhan123@gmail.com>
2024-02-06 10:06:41 +0530

    backend: update openai sdk

--- utils/gpt.js
+++ utils/gpt.js
@@ -1,4 +1,4 @@
-const { Configuration, OpenAIApi } = require('openai');
+const OpenAI = require('openai');
 const dotenv = require('dotenv');
 
 // load env vars
@@ -7,29 +7,28 @@ dotenv.config({ path: '../.env' });
 let openai;
 const initializeOpenAI = async () => {
   try {
-    const configuration = new Configuration({
-      apiKey: process.env.OPENAI_API_KEY,
+    openai = new OpenAI({
+      apiKey: process.env.OPENAI_API_KEY, 
     });
-    openai = new OpenAIApi(configuration);
   } catch (err) {
     console.log(err);
   }
 };
 
-const model_engine = 'text-davinci-003';
+const model_engine = 'gpt-3.5-turbo';
 
 const get_response = async (user_input) => {
   try {
     //  Get the response from GPT-3
-    let response = await openai.createCompletion({
+    let response = await openai.chat.completions.create({
       model: model_engine,
-      prompt: user_input,
+      messages: [{ role: 'user', content: user_input }],
       max_tokens: 2000,
       n: 1,
     });
 
     // Extract the response from the response object
-    let response_text = response.data.choices[0].text;
+    let response_text = response.choices[0].message.content;
 
     let chatbot_response = response_text.trim();
     
